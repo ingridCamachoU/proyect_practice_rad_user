0489ed88ae37b20132d9132b52594c1bb124d9cc
Ingrid Camacho <injuca28@hotmail.com>
2023-07-27 22:05:22 -0500

    commit

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,25 +1,116 @@
+import { useState } from "react";
+import { useForm } from "../hooks/useForm";
 
 const Form = () => {
+
+    const initialForm ={
+        "name": "",
+        "email": "",
+        "phone": "",
+        "message": "",
+    };
+
+    const [formData, handleChange, handleReset, setFormData] = useForm (initialForm);
+
+    const [errors, setErrors] = useState({});
+
+    const onValidate = (formData)=>{
+        let errors = {};
+        let regexName = /^([0-9-A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]){2,20}$/;
+        let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+        let regexPhone = /^([0-9-A-Za-zÑñÁáÉéÍíÓóÚúÜüs]){5,15}$/;
+        let regexMessage = /^.{1,255}$/;
+
+        if (!formData.name.trim()){
+            errors.name= 'El campo "Nombre" no debe ser vacio.';
+        }else if(!regexName.test(formData.name)){
+            errors.name= 'El campo "Nombre" es incorrecto.';
+        }
+
+        if (!formData.email.trim()){
+            errors.email= 'El campo "Email" no debe ser vacio.';
+        }else if(!regexEmail.test(formData.email)){
+            errors.email= 'El campo "Email" es incorrecto.';
+        }
+
+        if (!formData.phone.trim()){
+            errors.phone= 'El campo "Celular" no debe ser vacio.';
+        }else if(!regexPhone.test(formData.phone)){
+            errors.phone= 'El campo "Celular" es incorrecto, no puede tener espacios en blanco y debe tener más de 5 caracteres';
+        }
+
+        if (!formData.message.trim()){
+            errors.message= 'El campo "Mensaje" no debe ser vacio.';
+        }else if(!regexMessage.test(formData.message)){
+            errors.message= 'El campo "Mensaje" es incorrecto.';
+        }
+        return errors;
+    };
+
+    const err= onValidate(formData);
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        setErrors(err);      
+
+        if (Object.keys(errors).length === 0){
+            setFormData(initialForm);
+            setErrors('');
+            handleReset();
+        }
+
+    };
+
   return (
         <>
-            <form className="form-Cont">
+            <form className="form-Cont" onSubmit={handleSubmit}>
                 <h2 className="text-blue">Escríbenos</h2>
 
                 <div className="row">
                     <div className="col-6 ">
-                        <input type="text" name="" placeholder="Nombre" className="form-control" />
+                        <input 
+                        type="text" 
+                        name="name" 
+                        required
+                        placeholder="Nombre" 
+                        className="form-control" 
+                        onChange={handleChange} 
+                        value={formData.name}/>
+                        {errors.name && <p className="text-danger">{errors.name}</p>}
                     </div>
                     <div className="col-6 ">
-                        <input type="email" name="" placeholder="Email" className="form-control" />
+                        <input 
+                        type="email" 
+                        name="email" 
+                        required 
+                        placeholder="Email"
+                        className="form-control" 
+                        onChange={handleChange} 
+                        value={formData.email}/>
+                        {errors.email && <p className="text-danger">{errors.email}</p>}
                     </div>
                 </div>
 
-                <input type="number" name="" placeholder="Número Celular" className="form-control" />
+                <input 
+                type="number" 
+                name="phone" 
+                required
+                placeholder="Número Celular" 
+                className="form-control" 
+                onChange={handleChange} 
+                value={formData.phone}/>
+                {errors.phone && <p className="text-danger">{errors.phone}</p>}
 
-                <textarea placeholder="Mensaje" className="form-control" />
+                <textarea 
+                placeholder="Mensaje" 
+                name="message" 
+                required
+                className="form-control" 
+                onChange={handleChange} 
+                value={formData.message}/>
+                {errors.message && <p className="text-danger">{errors.message}</p>}
 
                 <input type="submit"  value="Enviar" className=" btnEnviar"/>
-
 
             </form>
         </>
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -2,21 +2,29 @@ import { useContext, useEffect, useState } from "react";
 import { DarkMode } from "../../Context/DarkMode";
 import { Link } from "react-router-dom";
 import './Products.css';
+import Pagination from "../Pagination";
+import sinImagen from '../../assets/img/sin-imagen.jpeg'
+
 
 const Products = () => {
 
     const [dataproducts, setDataProducts] = useState([]);
     const [search, setSearch] = useState('');
+    const [productsPage, setProductsPage] = useState(3);
+    const [currentPage, setCurrentPage] = useState(1);
 
   const {darkMode} = useContext(DarkMode);
 
   const urlProduct = "http://localhost:8000/api/v1/products/";
+
+  const totalProducts= dataproducts.length ; 
 
    const showData = async() =>{
     const response = await fetch(urlProduct)
     const data= await response.json();
     setDataProducts(data)
    }
+
 
    const handleChange = (e) =>{
         setSearch(e.target.value)
@@ -51,18 +59,18 @@ const Products = () => {
                 </div>
 
                 <div className="contenedor-product">{
-
                         results.map( (product) =>( 
                         <div key={product.id} className="product-box">
                             <h4 className="mt-3">{product.name}</h4>
                             <div className="cont-img">
-                                <img  className="img-product" src={product.images} />
+                                <img  className="img-product" src={product?.images.length > 0 ? product?.images : sinImagen} />
                             </div>
                             <p className="mt-2">{product.mark_model.name}({product.mark_model.mark.name})</p>
                         </div>
                     ))}       
                 </div>
             </div>
+            <Pagination productsPage={productsPage} currentPage={currentPage} setCurrentPage={setCurrentPage} totalProducts={totalProducts}/>
         </section> 
     );
 }
